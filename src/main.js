502943115498995f732636341aa0184371f85797
Chaiyawat Nunes <thaiberius.code@gmail.com>
2015-04-30 19:18:56 -0700

    Prettier
    * Spaces are key
    * Single Quotes

--- src/main.js
+++ src/main.js
@@ -1,30 +1,30 @@
 //Development script, remember to convert to production, but create a copy first
-var init = function(){//Starting blocks of code
-    game.container.style.backgroundImage = "bin/bert_1.png";
+var init = function() {//Starting blocks of code
+    game.container.style.backgroundImage = 'bin/bert_1.png';
 };
 var shootButton = {
-  container : document.getElementById("attack"),
-  fire : function(){
+  container : document.getElementById('attack'),
+  fire : function() {
     //Shows it on the display
   }
 };
 var game = {
   container : document.getElementById('game'),
-  context : document.getElementById('game').getContext("2d"),
+  context : document.getElementById('game').getContext('2d'),
   width : window.innerwidth,
   height : window.innerheight,
 };
 var press = function(button){
-  if(button === "left"){
-    console.log("Left button pressed");
-  }else if(button === "right"){
-    console.log("Right button pressed");
-  }else if(button === "SHOOT"){
-    console.log("Shoot button pressed");
+  if(button === 'left'){
+    console.log('Left button pressed');
+  } else if(button === 'right'){
+    console.log('Right button pressed');
+  } else if(button === 'SHOOT'){
+    console.log('Shoot button pressed');
     shootButton.fire();
   }
 };
-/*On two occasions I have been asked, ‘Pray, Mr. Babbage, if
+/* On two occasions I have been asked, ‘Pray, Mr. Babbage, if
 you put into the machine wrong figures, will the right answers
 come out?’ […] I am not able rightly to apprehend the kind of
 confusion of ideas that could provoke such a question.”
