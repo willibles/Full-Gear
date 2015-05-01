0506806bcc4a442756b04fa990c341d1755a2091
Chaiyawat Nunes <thaiberius.code@gmail.com>
2015-04-30 21:04:51 -0700

    Quotes
    @ak65234

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // Development script, remember to convert to production, but create a copy first
 // Starting blocks of code
 var init = function() { 
-    game.container.style.backgroundImage = 'bin/bert_1.png';
+    game.container.style.backgroundImage = "bin/bert_1.png";
 };
 
 // On-screen shoot button
 var shootButton = {
-  container : document.getElementById('attack'),
+  container : document.getElementById("attack"),
   fire : function() {
     //Shows it on the display
   }
@@ -14,21 +14,21 @@ var shootButton = {
 
 // Define the game
 var game = {
-  container : document.getElementById('game'),
-  context : document.getElementById('game').getContext('2d'),
+  container : document.getElementById("game"),
+  context : document.getElementById("game").getContext("2d"),
   width : window.innerwidth,
   height : window.innerheight,
 };
 
 // Press
 var press = function (button) {
-  var key = event.charCode.toString;
-  if(button === 'left' || key === '65' || key === '37') {
-    console.log('Left button pressed');
-  } else if(button === 'right' || key === '68' || key === '39') {
-    console.log('Right button pressed');
-  } else if(button === 'SHOOT' || key === '13' || key === '32' ) {
-    console.log('Shoot button pressed');
+  var key = event.charCode;
+  if(button === "left" || key === 65 || key === 37) {
+    console.log("Left button pressed");
+  } else if(button === "right" || key === 68 || key === 39) {
+    console.log("Right button pressed");
+  } else if(button === "SHOOT" || key === 13 || key === 32 ) {
+    console.log("Shoot button pressed");
     shootButton.fire();
   }
 };
@@ -38,4 +38,4 @@ you put into the machine wrong figures, will the right answers
 come out?’ […] I am not able rightly to apprehend the kind of
 confusion of ideas that could provoke such a question.”
 —Charles Babbage, Passages from the Life of a Philosopher
-(1864)*/
+(1864) */
